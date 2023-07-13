0e5a70050c5968afc803b5911662054c184a4e79
Yihao Zhu <1069474952@qq.com>
2023-07-13 17:04:14 +0800

    finish the first version

---
+++ accounts/manage_account.py
@@ -0,0 +1,105 @@
+import sqlite3
+import random
+import string
+import sys
+
+USER_DB_ABSOLUTE_PATH = '/Users/thuzyh/Documents/hci/Inception/annotation/accounts/user_accounts.db'
+
+# Function to generate a random 8-digit password
+def generate_password():
+    password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    return password
+
+# Function to create a new user account
+def create_account(username):
+    username = username +'_'+''.join(random.choices(string.ascii_letters, k=2))
+    # Generate a random password
+    password = generate_password()
+    
+    # Save the user account to the database
+    conn = sqlite3.connect(USER_DB_ABSOLUTE_PATH)  # Connect to the database file
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    conn.commit()
+    conn.close()
+
+    return username,password
+
+# Function to validate a user's credentials
+def validate_user(username, password):
+    conn = sqlite3.connect(USER_DB_ABSOLUTE_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    user = cursor.fetchone()
+    conn.close()
+
+    if user is None:
+        return False  # User not found
+
+    if user[2] == password:
+        return True  # Password is correct
+
+    return False  # Password is incorrect
+
+# Function to print all user information
+def print_all_users():
+    conn = sqlite3.connect(USER_DB_ABSOLUTE_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    conn.close()
+
+    for user in users:
+        print("Username:", user[1])
+        print("Password:", user[2])
+        print()
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python manage.py [command]")
+        print("Available commands:")
+        print("- create <username>: Create a new user account")
+        print("- validate <username> <password>: Validate user credentials")
+        print("- print: Print all user information")
+        return
+
+    command = sys.argv[1]
+
+    if command == "create":
+        if len(sys.argv) < 3:
+            print("Please provide a username")
+            return
+
+        username = sys.argv[2]
+        username,password = create_account(username)
+        print("Account created successfully!")
+        print(f"Username: {username}, Password: {password}")
+        print()
+
+    elif command == "validate":
+        if len(sys.argv) < 4:
+            print("Please provide a username and password")
+            return
+
+        username = sys.argv[2]
+        password = sys.argv[3]
+        if validate_user(username, password):
+            print("Credentials are correct!")
+        else:
+            print("Credentials are incorrect!")
+        print()
+
+    elif command == "print":
+        print("All users:")
+        print_all_users()
+
+    else:
+        print("Invalid command")
+        print("Available commands:")
+        print("- create <username>: Create a new user account")
+        print("- validate <username> <password>: Validate user credentials")
+        print("- print: Print all user information")
+
+if __name__ == '__main__':
+    # parse the args to know which function to use:
+    main()
--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,6 @@
 from question_generater import QuestionGenerater
+from accounts.manage_account import validate_user
+import os
 
 class DataManager:
     # taking control of the user dataset and the question dataset
@@ -9,7 +11,7 @@ class DataManager:
         if id=='admin' and password=='admin':
             return True
         else:
-            return False
+            return validate_user(id,password)
     
     def generate_next_question(self):
         return self.question_generater.get_1v3_question()
@@ -20,6 +22,9 @@ class DataManager:
             f.write(f"{example_instance},{example_feasibility},{belief_rating},{belief_change_rating},{statements}\n")
 
     def get_user_finished_annotation_cnt(self,user_id) -> int:
+        # if no user data file created, return 0
+        if not os.path.exists(f"data/{user_id}.csv"):
+            return 0
         # load the number of line in the user data file
         with open(f"data/{user_id}.csv",'r') as f:
             return len(f.readlines())
