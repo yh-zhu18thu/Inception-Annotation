54b424cc50cd3cc50880312eea9c346df931d386
Yihao Zhu <1069474952@qq.com>
2023-07-30 23:40:36 +0800

    modified to 1+1v1

--- data_manager.py
+++ data_manager.py
@@ -6,7 +6,6 @@ class DataManager:
     # taking control of the user dataset and the question dataset
     def __init__(self) -> None:
         self.question_generater = QuestionGenerater()
-        self.item_cnt = 1
 
     def validate_user(self,id,password):
         if id=='admin' and password=='admin':
@@ -14,16 +13,32 @@ class DataManager:
         else:
             return validate_user(id,password)
     
-    def generate_next_question(self):
-        return self.question_generater.get_question(self.item_cnt)
+    def generate_next_question(self,user_id):
+        if user_id not in self.question_statistic:
+            self.question_statistic[user_id] = []
+        return self.question_generater.get_question(user_id)
     
     def generate_default_question(self):
         return self.question_generater.get_default_question()
     
-    def save_current_selection(self,user_id,example_instance,example_feasibility,instance,belief_rating,belief_change_rating,statements):
+    def save_current_selection(self,data):
         # create a new file with user id if not exist, append a line to the file
+        user_id = data["user_id"]
+        type = data["type"]
+        
         with open(f"data/{user_id}.csv",'a') as f:
-            f.write(f"{example_instance},{example_feasibility},{instance},{belief_rating},{belief_change_rating},{statements}\n")
+            if type=='1':
+                instance = data["instance"]
+                belief_rating = data["belief_rating"]
+                f.write(f"{type},{instance},{belief_rating}\n")
+            elif type=='1v1':
+                example_instance = data["example_instance"]
+                example_feasibility = data["example_feasibility"]
+                instance = data["instance"]
+                belief_rating = data["belief_rating"]
+                statements = data["statements"]
+                f.write(f"{type},{example_instance},{example_feasibility},{instance},{belief_rating},{statements}\n")
+
 
     def get_user_finished_annotation_cnt(self,user_id) -> int:
         # if no user data file created, return 0
--- question_generater.py
+++ question_generater.py
@@ -8,6 +8,7 @@ class QuestionGenerater:
         self.instance_dataset_path = "src/instances_with_similarity_Robuddy.csv"
         self.load_instance_dataset()
         self.instance_cnt = len(self.df)
+        self.user_question_statistic = {}
         
 
     def load_instance_dataset(self):
@@ -41,7 +42,7 @@ class QuestionGenerater:
     def get_default_question(self):
         return {"type":"1v1","example_instance":"","feasibility":True,"instance":[""]}
     
-    def get_question(self,item_cnt):
+    def get_1v1_question(self,user_id):
         instance_id = self.pick_random_example_id()
         #print(df.iloc[instance_id])
         instance_row = self.df.iloc[instance_id]
@@ -52,7 +53,29 @@ class QuestionGenerater:
             similarity = float(instance_row.loc[f"similarity_{i}"])
             similarity_dict[i] = similarity
         sorted_similarity_list = sorted(similarity_dict.items(),key=lambda x:x[1],reverse=True)[1:]
-        picked_list = self.priorized_pick_from_sorted_list(sorted_similarity_list,item_cnt)
+        picked_list = self.priorized_pick_from_sorted_list(sorted_similarity_list)
         picked_index_list = [sorted_similarity_list[rank][0] for rank in picked_list]
         expression_list = [self.df.iloc[id].loc["expression"] for id in picked_index_list]
+        self.user_question_statistic[user_id]["single_commands"].add(expression_list[0])
         return {"type":"1v1","example_instance":instance_expression,"feasibility":correct,"instance":expression_list[0]}
+    
+    def get_1_question(self,user_id):
+        # randomly pick one from single commands and delete it from the set
+        single_commands = self.user_question_statistic[user_id]["single_commands"]
+        single_commands = list(single_commands)
+        picked_index = random.randint(0,len(single_commands)-1)
+        picked_command = single_commands[picked_index]
+        single_commands.remove(picked_command)
+        self.user_question_statistic[user_id]["single_commands"] = set(single_commands)
+        return {"type":"1","instance":picked_command}
+
+    
+    def get_question(self,user_id):
+        if user_id not in self.user_question_statistic:
+            self.user_question_statistic[user_id] = {"single_commands":set()}
+        #0.5 true 0.5 false
+        random_number = random.random()
+        if random_number<0.5 and len(self.user_question_statistic[user_id]["single_commands"])>0:
+            return self.get_1_question(user_id)
+        else:
+            return self.get_1v1_question(user_id)
